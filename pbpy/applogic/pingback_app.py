d466bab77ce1696059f43303977c7cec5b623f1c
Gatsby Lee <thewayofthining@gmail.com>
2018-10-29 12:10:21 -0700

    update pingback_app to use redis SortedSet

--- pbpy/applogic/pingback_app.py
+++ pbpy/applogic/pingback_app.py
@@ -2,13 +2,45 @@
 @author Gatsby Lee
 @since 2018
 
-@note: hard-coded storage type
+@note: hard-coded storage type for Redis
 """
+import logging
+import time
 
-REDIS_KEY_TEMPLATE = 'list:{}_completed_tasks'
+from pbpy.cexception import IntegrityError
+
+LOGGER = logging.getLogger(__name__)
+REDIS_KEY_TEMPLATE = 'sset:{}_completed_tasks'
 
 class PingbackApp(object):
 
     @classmethod
-    def add_pingback(cls, storage_engine, task_id, post_id):
-        pass
+    def add_pingback(cls, storage_engine, pingback_src, pingback_entry):
+        """Add pingback entry into storage ( currently Redis )
+
+            Args:
+                storage_engine (obj): instance of storage_engine
+                pingback_src (str): used to generate storage namespace
+                pingback_entry (tuple): (task_id(str), post_id(str))
+            Returns: None
+            Exceptions:
+                If same pingback_entry exists, then IntegrityError raise.
+        """
+        LOGGER.debug("%s,%s", pingback_src, pingback_entry)
+        storage_namespace = REDIS_KEY_TEMPLATE.format(pingback_src)
+        # zadd: O(log(N)), sadd: O(1)
+        # SortedSet zadd options:
+        #   NX - Don't update already existing elements. Always add new elements.
+        sortedset_score = int(time.time())
+        # @note: To process pingback task in order of arrival, `SortedSet` is used.
+        #       If this use case is unncessary, it's good to change to `Set`
+        # redis-py 2.10.6 doesn't support `nx` option in zadd yet.
+        # @todo: Either wait until redis-py supports `nx` option
+        #           or build redis-py wrapper module.
+        # ret_value = storage_engine.zadd(storage_namespace, sortedset_score,
+        #                                 pingback_entry)
+        ret_value = storage_engine.execute_command('ZADD', storage_namespace, 'NX',
+                                                   sortedset_score, pingback_entry)
+        if ret_value == 0:
+            raise IntegrityError('%s already exists in %s' % \
+            (pingback_entry, storage_namespace))
